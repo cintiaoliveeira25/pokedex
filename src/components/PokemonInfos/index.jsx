51f3ab001a32bc6eb6601b1e93499ec71b882c2d
cintia.oliveira <cintia.oliveeira@hotmail.com>
2021-10-27 22:03:02 -0300

    exibe descrição e tipos de cada pokemon

--- src/components/PokemonInfos/index.jsx
+++ src/components/PokemonInfos/index.jsx
@@ -21,11 +21,15 @@ export function PokemonInfos() {
     const data = await response.json();
     setPokemon(data);
 
-    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.forms[0].name}`);
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
     const dataSpecies = await resp.json();
-    setPoke(dataSpecies);
+    const pokeDescription = await dataSpecies.flavor_text_entries
+      .filter((text) => text.language.name === "en")[0]
+      .flavor_text.replaceAll("\f", " ")
+      .replaceAll("POKéMON", "Pokémon");
+
+    setPoke({ ...dataSpecies, flavor_text_entries: pokeDescription });
     setIsLoading(false);
-    console.log(dataSpecies.flavor_text_entries);
   }
 
   useEffect(() => {
@@ -35,34 +39,40 @@ export function PokemonInfos() {
   return (
     <Container>
       <div className="poke-info">
-        <div className="title">
-          {isLoading ? (
-            "loading..."
-          ) : (
-            <>
+        {isLoading ? (
+          "loading..."
+        ) : (
+          <>
+            <div className="title">
               <span className="poke-name">{pokemon.forms[0].name}</span>
               <span className="poke-id">
                 {idSelected < 10 && `#00${idSelected}`}
                 {idSelected > 10 && idSelected < 99 && `#0${idSelected}`}
                 {idSelected > 99 && `#${idSelected}`}
               </span>
-            </>
-          )}
-        </div>
+            </div>
 
-        <div className="row">
-          <div className="left-side">
-            <img
-              src={`${pokemonImages}/${idSelected}.png`}
-              alt="pokemon"
-              className="background-image"
-            />
-          </div>
-          <div className="">
-            {/* {poke.flavor_text_entries[0].flavor_text} */}
-          </div>
-        </div>
-
+            <div className="row">
+              <div className="picture">
+                <img
+                  src={`${pokemonImages}/${idSelected}.png`}
+                  alt="pokemon"
+                  className="background-image"
+                />
+              </div>
+              <div className="information">
+                <p>{poke.flavor_text_entries}</p>
+                <div className="types">
+                  {pokemon.types.map((tipo) => (
+                    <div className={`element type-${tipo.type.name}`}>
+                      {tipo.type.name}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Container>
   );
